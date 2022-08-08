21dec3e6552c14cdfe290f9035819099b76164c5
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-08-08 12:34:32 +0300

    Full resposive base apparel project

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('.form_input')
+const email = document.getElementById('apparel_email')
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const emailVal = email.value
+    if(!validateEmail(emailVal)){
+        form.classList.add('error')
+    }else{
+        const errorMsg = document.querySelector('.error_msg')
+        errorMsg.innerText = 'Succesfuly submitted a valid Email'
+        errorMsg.style.color = 'green'
+        form.classList.add('success')
+    }
+})
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
